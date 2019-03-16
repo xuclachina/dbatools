e250cc1db20267fa91b937a9061fc7db831adee6
xuclachina <573242930@qq.com>
2019-03-16 18:32:51 +0800

    modify check_mysql

--- check_mysql.py
+++ check_mysql.py
@@ -216,7 +216,7 @@ s.table_name=t.table_name and s.index_name=t.index_name;"
         status_list = ['Uptime', 'Opened_files', 'Opened_table_definitions', 'Opened_tables', 'Max_used_connections',
                        'Threads_created', 'Threads_connected', 'Aborted_connects', 'Aborted_clients',
                        'Table_locks_waited', 'Innodb_buffer_pool_wait_free', 'Innodb_log_waits',
-                       'Table_locks_waited', 'Innodb_row_lock_waits', 'Innodb_row_lock_time_avg']
+                       'Innodb_row_lock_waits', 'Innodb_row_lock_time_avg', 'Binlog_cache_disk_use', 'Created_tmp_disk_tables']
         for status in status_list:
             try:
                 sql = ("show global status like '%s'" % status)
